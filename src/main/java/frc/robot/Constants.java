8276d1184dcd7e120e166f55dd064ed69c1397cd
Isabella Li <yilei.li@chca-oh.org>
2019-02-02 13:50:29 -0500

    replace contants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -23,8 +23,6 @@ public class Constants {
 	// this is where the USB stick is mounted on the RoboRIO filesystem.  
 	// You can confirm by logging into the RoboRIO using WinSCP
 	public static final String PRIMARY_LOG_FILE_PATH = "/media/sda1/logging";
-	public static final String ALTERNATE_LOG_FILE_PATH = "/media/sdb1/logging";
-	
-	
+    public static final String ALTERNATE_LOG_FILE_PATH = "/media/sdb1/logging";
     
 }
